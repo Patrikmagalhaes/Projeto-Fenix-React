c907a2ee0e10384766985f9ad5cf29ef18a72a84
cassia figueiredo <patrikmagalhaes@icloud.com>
2024-04-21 22:25:57 -0300

    primeiro commit

---
+++ src/Header.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {
+    Offcanvas, OffcanvasHeader, OffcanvasBody, Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink,
+    Button
+} from 'reactstrap';
+import './Header.css'
+import MenuIcon from './assets/images/menuIcon.png'
+import PhoenixLoo from './assets/images/phoenix.png'
+function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <header >
+            <Navbar light expand="md">
+                <img className='menu' src={PhoenixLoo}alt="" />
+                <h1 >Projeto Fênix</h1>
+                    <img onClick={toggle} className="d-md-none menu"src={MenuIcon}alt="" />
+                <Offcanvas isOpen={isOpen} toggle={toggle} direction="end" scrollable className="d-md-none">
+                    <OffcanvasHeader toggle={toggle}>
+                        Menu
+                    </OffcanvasHeader>
+                    <OffcanvasBody>
+                        <Nav vertical>
+                            <NavItem>
+                                <NavLink href="#">Sobre o Projeto</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Conquistas e Eventos</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Contato</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </OffcanvasBody>
+                </Offcanvas>
+                <Collapse isOpen={!isOpen} navbar className="d-none d-md-block">
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="#">Sobre o Projeto</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">Conquistas e Eventos</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#">Contato</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Home.jsx
@@ -0,0 +1,53 @@
+
+
+import Header from './Header.jsx'
+import CarouselSlide from './Carousel.jsx'
+import IconeBook from './assets/images/iconebook.png'
+import IconeBola from './assets/images/iconebola.png'
+import IconeTrofeu from './assets/images/iconetrofeu.png'
+import './Home.css'
+import './index.css'
+function Home() {
+
+  return (
+    <>
+
+      <Header />
+      <CarouselSlide />
+      <div id='inicio' className='card1'>
+        <h2>Projeto Fênix de <br />Esporte, <br />Cultura <br />e Lazer</h2>
+        <strong className='text-center'>Desde 2015</strong>
+        <button className='botao-laranja'>SOBRE O PROJETO</button>
+      </div>
+      <section id="conquistas">
+        <div>
+          <h2>Eventos e<br />Conquistas da<br /> Equipe </h2>
+          <p>Nos acompanhe!</p>
+          <button className='botao-laranja'>FIQUE POR DENTRO!</button>
+        </div>
+      </section>
+      <section id='conteudo'>
+        <div>
+          <h2>Conteúdo Didático</h2>
+          <h3>Aprenda, Pratique, Conquiste!</h3>
+          <div>
+            <img src={IconeBook} alt="" />
+            <img src={IconeBola} alt="" />
+            <img src={IconeTrofeu} alt="" />
+          </div>
+        </div>
+        <button className='botao-laranja'>LEIA MAIS!</button>
+      </section>
+      <section id='apoia-se'>
+        <div>
+          <h2>Faça Parte!</h2>
+          <h3>Seja um Aluno, Professor ou Apoiador</h3>
+          <p>Saiba como participar ou apoiar o Projeto Fênix</p>
+          <button  className='botao-laranja'>SAIBA MAIS!</button>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
